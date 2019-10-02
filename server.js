67dbb7a087020e5522c83bade163dbd8a36687d5
Remoguima <55773988+Remoguima@users.noreply.github.com>
2019-10-02 20:39:16 -0300

    a
    isso

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const people = require('./people.json');
+
 const app = express();
 
+app.set('view engine', 'pug');
 
-const server = app.listen(7000, () => {
-    console.log(`Servidor Rodando... → PORTA  ${server.address().port}`);
-  });
-
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+  res.render('index', {
+    title: 'Homepage',
+    people: people.profiles
+  });
+});
+
+app.get('/profile', (req, res) => {
+    const person = people.profiles.find(p => p.id === req.query.id);
+    res.render('profile', {
+      title: `About ${person.firstname} ${person.lastname}`,
+      person,
+    });
   });
 
-  
+const server = app.listen(7000, () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
